9e9c1dfabf3096a50df10b097b82a2ddae905884
Kosuke Shirakashi <k.shira86@gmail.com>
2013-05-04 02:41:57 +0900

    bp

--- backpropagation.cpp
+++ backpropagation.cpp
@@ -1,11 +1,7 @@
-//誤差逆伝搬法プログラムその2
-//Programmed by Kosuke SHIRAKASHI
-//Shima Lab. 2013
+//Back Propagation
 //訓練データの形式として
-//1行目に、データ数、入力数、出力数
+//1行目に、データ数、入力数、出力数を記述
 //2行目以降、入力値、出力値をスペースまたは改行で区切って記述。
-
-
 #include<iostream>
 #include<cmath>
 #include<fstream>
@@ -15,19 +11,17 @@
 #include<vector>
 #include<map>
 
-typedef std::pair<int,int> Pair;
-
 //訓練データ
 struct TrainingData{
-	std::vector<int> input;
-	std::vector<int> output;
+	std::vector< int > input;
+	std::vector< int > output;
 	TrainingData(int i, int o){
 		input = std::vector<int>(i);
 		output = std::vector<int>(o);
 	}
 };
 
-//各パラメータ
+//パラメータ
 struct params{
 	int num_learn, num_sample;//学習打ち切り回数、訓練データ数
 	int num_input, num_hidden, num_output;//入力素子数、隠れ層素子数、出力素子数
@@ -42,14 +36,19 @@ struct params{
 	}
 };
 
+//表示テキスト
+const std::string MSG[3] ={
+	"1.訓練データ入力\n2.学習\n3.試してみる\n4.パラメータ調整\n0.終了",
+	"hoge",
+	"piyo"
+};
+
 //訓練データ格納
 std::vector< TrainingData > target;
-//各素子の値を突っ込む。
-std::vector< double > x, h, y;
 //結線の重み。
+typedef std::pair< int,int > Pair;
 std::map< Pair, double > w1, w2;
-//逆伝搬量
-std::vector<double> h_back, y_back;
+
 //シグモイド関数
 double sigmoid( const double s , const double gain){
 	return 1 - (1 / ((double)1 + exp(gain * s)));
@@ -61,15 +60,7 @@ double d_rand(){
 	return (rand()%2 == 0) ? ret : -ret;
 }
 
-
-const std::string INTRODUCTION = "****************************************\n誤差逆伝搬法プログラム\nProgrammed by Kosuke SHIRAKASHI\nSHIMA Lab. 2013\n****************************************\n";
-
-const std::string MSG[3] ={
-	"1.データ入力\n2.学習\n0.終了",
-	"hoge",
-	"piyo"
-};
-
+//ファイルから訓練データを読み込む
 void set_data(params& param, const std::string filename = "training2.dat")
 {
 	using namespace std;
@@ -97,6 +88,7 @@ void set_data(params& param, const std::string filename = "training2.dat")
 	}
 
 	//訓練データを表示
+	printf("input training data from %s\n",filename.c_str());
 	for(int isample = 0; isample < param.num_sample; isample++){
 		cout <<"訓練データ No."<< isample+1 << endl;
 		cout <<"入力：";
@@ -121,9 +113,16 @@ void set_data(params& param, const std::string filename = "training2.dat")
 
 void learn(const params param)
 {
+
 	const int n_input = param.num_input, n_hidden = param.num_hidden, n_output = param.num_output;
 	const int n_learn = param.num_learn, n_sample = param.num_sample;
 	const double gain = param.s_gain, epsilon = param.epsilon, threshold_error = param.threshold_error;
+
+	//各素子の値
+	std::vector< double > x(n_input + 1), h(n_hidden + 1), y(n_output);
+	//逆伝搬量
+	std::vector< double > h_back(n_hidden), y_back(n_output);
+
 
 	//結線の重みを初期化
 	srand(time(NULL));
@@ -138,18 +137,11 @@ void learn(const params param)
 		}
 	}
 
-	//vector初期化
-	x.assign(n_input + 1, 0.0);
-	h.assign(n_hidden + 1, 0.0);
-	y.assign(n_output, 0.0);
-
-	y_back.assign(n_output,0.0);
-	h_back.assign(n_hidden,0.0);
-
 	//学習
-	for(int ilearn = 0; ilearn < n_learn; ilearn++){
-		double max_error = 0;
-
+	int ilearn;
+	double max_error;
+	for(ilearn = 0; ilearn < n_learn; ilearn++){
+		max_error = 0;
 		for(int isample = 0; isample < n_sample; isample++){
 			//順方向の動作
 			for(int i = 0; i < n_input; i++){
@@ -185,7 +177,8 @@ void learn(const params param)
 			if( error > max_error ){
 				max_error = error;
 			}
-			printf("学習回数 = %d, 訓練データNO.%d, 誤差 = %lf\n", ilearn, isample+1, error);
+
+			//printf("学習回数 = %d, 訓練データNO.%d, 誤差 = %lf\n", ilearn, isample+1, error);
 
 			//逆方向の動作
 			//出力層素子
@@ -215,14 +208,57 @@ void learn(const params param)
 			}
 		}
 
-		//誤差が十分小さくなったらループを抜ける。
+		//誤差が小さくなったらループを抜ける。
 		if( max_error < threshold_error ){
 			break;
 		}
 	}
+	printf("学習回数:%d, 誤差:%lf\n",ilearn, max_error);
 }
 
-//キー入力でうまいことやる関数。
+void execute(const params param)
+{
+	const int n_input = param.num_input, n_hidden = param.num_hidden, n_output = param.num_output;
+	const double gain = param.s_gain;
+	//各素子の値を入れるためのやつ
+	std::vector< double > x(n_input + 1), h(n_hidden + 1), y(n_output);
+
+	printf( "入力値はいかがなさいますか？(n = %d)\n>",n_input);
+	for(int i = 0; i < n_input; i++){
+		std::cin >> x[i];
+		while(!std::cin){
+			std::cout <<"ちゃんと入力しちくり〜\n>"<<std::flush;
+			std::cin.clear();
+			std::cin.ignore(INT_MAX, '\n');
+			std::cin >> x[i];
+		}
+	}
+	x[n_input] = 1.0; //閾値
+
+	//出力を求める。学習のとこのコピペなのでまとめたい。
+	//隠れ素子値の計算
+	for(int j = 0; j < n_hidden; j++){
+		double net_input = 0;
+		for(int i = 0; i < n_input + 1; i++){
+			net_input += w1[ Pair(i,j) ] * x[i];
+		}
+		h[j] = sigmoid(net_input, gain);
+	}
+
+	//出力値の計算
+	printf("--出力--\n");
+	for(int j = 0; j < n_output; j++){
+		double net_input = 0;
+		for(int i = 0; i < n_hidden + 1; i++){
+			net_input += w2[ Pair(i,j) ] * h[i];
+		}
+		y[j] = sigmoid(net_input, gain);
+		printf("%d:%lf\n", j+1, y[j]);
+	}
+	printf("--------\n");
+}
+
+//キー入力を指定した値の範囲に制限。
 template<class T> T input_key(T begin, T end){
 	T value;
 	std::cin >> value;
@@ -236,20 +272,29 @@ template<class T> T input_key(T begin, T end){
 	return value;
 }
 
+//パラメータの設定
+void config(params* param){
+	printf("隠れ層素子数(現在%d) > ",param->num_hidden);
+	param->num_hidden = input_key<int>(1,100);
+	printf("シグモイド関数ゲイン(現在%lf)", param->s_gain);
+	param->s_gain = input_key<double>(-10.0, 10.0);
+	printf("学習重みε(現在%lf)",param->epsilon);
+	param->epsilon = input_key<double>(0,10.0);
+	printf("許容誤差(現在%lf)",param->threshold_error);
+	param->threshold_error = input_key<double>(0.00001,100);
+}
+
 int main()
 {
 	using namespace std;
 	params p;
 	int key = -1; //キー入力
 	while(key != 0){
-		//system("reset");//windowsならsystem("cls")
-		cout << INTRODUCTION << endl;
 		cout <<MSG[0] << endl;
-		key = input_key<int>(0,2);
+		key = input_key<int>(0,4);
 		switch(key){
 		case 1:
 			//データ入力
-			cout << "input data from file." << endl;
 			set_data(p);
 			break;
 		case 2:
@@ -258,13 +303,17 @@ int main()
 			learn(p);
 			break;
 		case 3:
+			//試してみる
+			execute(p);
+			break;
+		case 4:
+			//パラメータの調整
+			config(&p);
+			cout << p.s_gain << endl;
 			break;
 		case 0:
 			cout <<"終了します"<<endl;
 			break;
 		}
-		cout <<"何かキー入力してエンタープリーズ."<<endl;
-		char c;
-		cin >> c;
 	}
 }
